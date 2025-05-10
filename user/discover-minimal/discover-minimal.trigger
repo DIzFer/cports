38b087c00c103b21c92457e110c9f4c3ac7a4e82
David Izquierdo <david@izquierdofernandez.com>
2025-05-11 01:56:18 +0200

    funny discover trigger

---
+++ user/discover-minimal/discover-minimal.trigger
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm /usr/lib/qt6/plugins/discover/fwupd-backend.so
+echo "Removed fwupd backend from discover's search path"
---
+++ user/discover-minimal/template.py
@@ -0,0 +1,8 @@
+pkgname = "discover-minimal"
+pkgver = "0"
+pkgrel = 0
+build_style = "meta"
+triggers = ["/usr/lib/qt6/plugins/discover"]
+pkgdesc = "Trigger to remove non-flatpak backends from discover"
+license = "custom:none"
+url = "http://localhost"
